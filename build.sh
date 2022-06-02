db28dfed8132dab5706fde2c720978ac6f06df70
William Blew <william@kulian.org>
2022-06-01 19:13:10 -0700

    create Dockerfile.3.16 based on Dockerfile.v3.12
    create baseline-alphine:v3.16 based on v3.12.
    Ignored v3.9 thru 3.12, they have security issues, per Trivy scan.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for ver in 9 10 11 12 ; do docker build --pull --no-cache --force-rm=true -t baseimage-alpine:v3.$ver -f Dockerfile.v3.$ver . ; done
+for ver in 16 ; do docker build --pull --no-cache --force-rm=true -t baseimage-alpine:v3.$ver -f Dockerfile.v3.$ver . ; done
